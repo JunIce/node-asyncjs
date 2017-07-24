cdb8b276bc9629e800ffa7c8172fbe807e18c504
JunIce <east.qrj@gmail.com>
2017-07-24 10:03:21 +0800

    concat

--- concat.js
+++ concat.js
@@ -13,4 +13,36 @@ async.concat(arr , function(item, cb) {
     console.info('catch err: ' + err);  
 });  
 
+async.concat(['aa','bb'], function(i, callback){
+	setTimeout(function(){
+		callback(null, [i,i]);
+	},100)
+}, function(err, data){
+	console.error(err);
+	console.log('result: ' + data);
+	console.log(typeof data);
+});
 
+var data = {
+    aaa: [11,22,33],
+    bbb: [44,55],
+    ccc: 66
+};
+
+var obj = [
+	{name : 'aaa', delay : 100},
+	{name : 'bbb', delay : 200},
+	{name : 'ccc', delay : 300},
+
+];
+
+async.concat(obj, function(i, callback){
+	setTimeout(function(){
+		if(i.name == 'ccc') callback('error'); //如果中途出错，则把错误以及已经完成的操作的结果交给最后callback。未执行完的则忽略。
+		else callback(null, data[i.name]);
+	},i.delay);
+}, function(err, data){
+	console.error(err);
+	console.log('result: ' + data);
+	console.log(typeof data);
+});
